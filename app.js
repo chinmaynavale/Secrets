8514830953be30484856f638bfb7a15350507701
Chinmay Navale <chinmaynavale9300@gmail.com>
2021-07-12 07:59:47 +0530

    Adding starting files

---
+++ app.js
@@ -0,0 +1,15 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(process.env.PORT || 3000);
